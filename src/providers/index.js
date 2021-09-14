d7dd030e8b5d055dfb2f4e0a2d809f1fe75ddd6f
JonatanPSilva <JonatanPSilva147@hotmail.com>
2021-09-14 18:52:26 -0300

    First Commit, feature Modal, implementation Provider and component Modal

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,10 +1,11 @@
 import { AutheticationProvider } from "./authentication";
+import { ModalProvider } from "./Modal";
 
 const Providers = ({ children }) => {
-	return (
-		<AutheticationProvider>
-            {children}
-		</AutheticationProvider>
-	);
+  return (
+    <AutheticationProvider>
+      <ModalProvider>{children}</ModalProvider>
+    </AutheticationProvider>
+  );
 };
 export default Providers;
