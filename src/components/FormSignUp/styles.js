c236e1be4c62039bf61864d727e4aadb22ee8350
Iasmim Oliveira <oliv.iasmim@gmail.com>
2021-09-14 19:03:11 -0300

    style(formsignup): increasing the width of the form inputs

--- src/components/FormSignUp/styles.js
+++ src/components/FormSignUp/styles.js
@@ -4,6 +4,9 @@ export const Form = styled.form`
     display: flex;
     flex-direction: column;
     gap:10px;
+    @media (min-width: 1024px){
+        width: 340px;
+    }
 `
 
 export const Label = styled.label`
