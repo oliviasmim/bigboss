da01c09f89eecdd8e2e5e0fcf648f8e186b20db8
Iasmim Oliveira <oliv.iasmim@gmail.com>
2021-09-14 22:05:45 +0000

    Revert "Merge branch 'feature/PageProfile' into 'master'"
    This reverts merge request !26

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,13 +6,5 @@ export default createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
         outline: 0;
-        font-family: "Inter", sans-serif;
-    }
-
-    :root{
-        --white:#f5f5f5;
-        --black:#0c0d0d;
-        --white-gray:#EAF0FC;
-        --blue: #134087;
     }
 `
