2fd1e4f1b0f0a8eb310318bfa9193dde63f41850
André Luiz S. Vilarinho <andluizv@msn.com>
2021-09-15 17:20:15 -0300

    style(projectstable): cleaning white spaces and commented code

--- src/components/LastProjectsTable/index.js
+++ src/components/LastProjectsTable/index.js
@@ -2,46 +2,17 @@ import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { DataGrid } from "@material-ui/data-grid";
 import { useUserContracts } from "../../providers/userContracts";
-import {getLettersAvatarSrc} from "../../services/avatarLetters"
+import { getLettersAvatarSrc } from "../../services/avatarLetters";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    //height: 300,
-    width: "100%",
-    // marginLeft: "10%",
-    //marginTop: 0,
-    display: "flex",
-
-    backgroundColor: "white",
-
-    // [theme.breakpoints.up(768)]: {
-    //   marginTop: 35,
-    //   marginLeft: "40%",
-    // },
-
-    // [theme.breakpoints.up(1024)]: {
-    //   marginLeft: "15%",
-    // },
-  },
+	root: {
+		width: "100%",
+		display: "flex",
+        backgroundColor: "white",
+	},
 }));
 
 const columns = [
-	// {
-	// 	field: "name",
-	// 	headerName: "ava",
-	// 	width: 220,
-	// 	renderCell: (params) => {
-	// 		return (
-	// 			<>
-	// 				<img
-	// 					src={getOneLetterAvatarSrc(params.row.client.name)}
-	// 					alt="avatar"
-	// 				/>
-	// 				<span>&nbsp;{params.row.client.name}</span>
-	// 			</>
-	// 		);
-	// 	},
-	// },
 	{
 		field: "name",
 		headerName: "Cliente",
@@ -51,16 +22,13 @@ const columns = [
 					<img
 						src={getLettersAvatarSrc(params.row.client.name)}
 						alt="avatar"
-                        style={{width: "40px", borderRadius: "100%"}}
+						style={{ width: "40px", borderRadius: "100%" }}
 					/>
 					<span>&nbsp;{params.row.client.name}</span>
 				</>
 			);
 		},
 		width: 200,
-		// valueFormatter: (params) => {
-		// 	return params.row.client.name;
-		// },
 	},
 	{
 		field: "client",
@@ -126,111 +94,25 @@ const columns = [
 	},
 ];
 
-// const rows = [
-//   {
-//     id: 1,
-//     customer: "Lannister",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.05.2021",
-//     FinishDate: "25.07.2021",
-//     status: "Concluído",
-//   },
-//   {
-//     id: 2,
-//     customer: "Marcos Silva",
-//     cpf: "032.853.527-22",
-//     service: "Web-Page",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Pendente",
-//   },
-//   {
-//     id: 3,
-//     customer: "Carlos Roberto",
-//     cpf: "032.853.527-22",
-//     service: "Game",
-//     StartDate: "25.03.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Concluído",
-//   },
-//   {
-//     id: 4,
-//     customer: "Ricardo Santos Silva",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Concluído",
-//   },
-//   {
-//     id: 5,
-//     customer: "José ricardo Santos",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Pendente",
-//   },
-//   {
-//     id: 6,
-//     customer: "Lannister",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Pendente",
-//   },
-//   {
-//     id: 7,
-//     customer: "Lannister",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Pendente",
-//   },
-//   {
-//     id: 8,
-//     customer: "Fabiane Carina Santos",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Pendente",
-//   },
-//   {
-//     id: 9,
-//     customer: "João Roberto Barroso",
-//     cpf: "032.853.527-22",
-//     service: "APP",
-//     StartDate: "25.02.2021",
-//     FinishDate: "25.04.2021",
-//     status: "Concluído",
-//   },
-// ];
+const LastProjectsTable = ({ maxRows, hideFooter, newRows }) => {
+	const { userContracts } = useUserContracts();
 
-
-const LastProjectsTable = ({maxRows, hideFooter}) => {
-    const { userContracts } = useUserContracts()
-    
-    const classes = useStyles();
-  return (
+	const classes = useStyles();
+	return (
 		<div className={classes.root}>
 			{userContracts.length && (
 				<DataGrid
-					rows={userContracts}
+					rows={newRows || userContracts}
 					columns={columns}
 					pageSize={maxRows}
 					autoPageSize
 					autoHeight
-					hideFooterPagination = {hideFooter}
-					// checkboxSelection
+					hideFooterPagination={hideFooter}
 					disableSelectionOnClick
 				/>
 			)}
 		</div>
-  );
+	);
 };
 
 export default LastProjectsTable;
