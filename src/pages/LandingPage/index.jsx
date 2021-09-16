395d6fadd76e8529238414f3da3e7de5386b409a
Iasmim Oliveira <oliv.iasmim@gmail.com>
2021-09-16 11:57:23 -0300

    feat(landingpage): creating strategies section

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,12 +1,18 @@
 import { useHistory } from "react-router";
+import Strategies from "../../components/Home/Strategies";
+
 
 const LandingPage = () => {
 	const history = useHistory();
     return (
+		<>
 		<div style={{display: "grid", placeItems: "center"}}>
             <button onClick={()=> history.push("/login")}>Login ={">"}</button>
             <button onClick={()=> history.push("/signup")}>Cadastro ={">"}</button>
 		</div>
+		<Strategies />
+		</>
+		
 	)
 }
 
